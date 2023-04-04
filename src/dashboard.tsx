26855d8fb96bddc467f83864a461c55dc7dcbc15
Luciano trujillo <luciano.trujillo@innovid.com>
2023-04-04 00:20:59 -0300

    obtain jwtToken of firebase

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -1,9 +1,20 @@
 import { Card, CardContent, CardHeader, Button } from '@mui/material';
-import { CreateButton, TopToolbar } from 'react-admin';
+import { CreateButton, TopToolbar, useAuthProvider } from 'react-admin';
+import React from 'react';
 
-export const Dashboard = () => (
-  <Card>
-    <CardHeader title="Bienvenido a FiuFit - Modo administrador" />
-    <CardContent>Toda la información se encuentra disponible</CardContent>
-  </Card>
-);
+export const Dashboard = () => {
+  const auth = useAuthProvider();
+  React.useEffect(() => {
+    auth.getJWTToken().then((res: any) => {
+      // PORFIN, aca esta el token jaja. Hay que pasarle esto al backend para que verifique, junto con mas datos
+      console.log(res);
+    });
+  }, []);
+
+  return (
+    <Card>
+      <CardHeader title="Bienvenido a FiuFit - Modo administrador" />
+      <CardContent>Toda la información se encuentra disponible</CardContent>
+    </Card>
+  );
+};
