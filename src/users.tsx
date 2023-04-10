a80314dc20ed728ce29662f1db95ac4810f04169
mpiligaddi <mgaddi@fi.uba.ar>
2023-04-10 16:40:08 -0300

    Add pagination

--- src/users.tsx
+++ src/users.tsx
@@ -1,10 +1,26 @@
-import { Datagrid, EmailField, List, TextField } from 'react-admin';
+import {
+  Datagrid,
+  DateField,
+  EmailField,
+  ImageField,
+  List,
+  Pagination,
+  TextField,
+} from 'react-admin';
 
 export const UserList = () => (
-  <List>
-    <Datagrid rowClick="edit">
+  <List
+    perPage={5}
+    pagination={<Pagination rowsPerPageOptions={[5, 10, 20, 30]} />}
+  >
+    <Datagrid>
+      <TextField source="id" />
+      <ImageField source="avatar" sx={{ my: -2 }} />
       <TextField source="name" />
       <EmailField source="email" />
+      <DateField source="createdAt" />
+      <DateField source="updatedAt" />
+      <TextField source="state" />
     </Datagrid>
   </List>
 );
