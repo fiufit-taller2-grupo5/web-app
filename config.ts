08ab0607ec8975c6e602d91ac06b23ae64e7395a
merivazquezn <mvazquezn@fi.uba.ar>
2023-07-03 23:43:06 -0300

    adding prod

--- config.ts
+++ config.ts
@@ -5,5 +5,5 @@ export const API_USER_URL =
 
 export const API_TRAININGS_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://api-gateway-prod-szwtomas.cloud.okteto.net/training-service/api'
+    ? 'https://api-gateway-prod2-szwtomas.cloud.okteto.net/training-service/api'
     : 'http://localhost:3000/training-service/api';
